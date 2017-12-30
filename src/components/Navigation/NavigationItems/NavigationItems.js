dc881a82d1d9e5bc18fa417fa355633ab66dfc0a
Nick Forlivio <whackdev@gmail.com>
2017-12-29 20:03:24 -0700

    add browser router module to index.js & change navigation items from a tags to NavLink

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,8 +6,8 @@ import classes from './NavigationItems.css'
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/" active>Burger Builder</NavigationItem>
-    <NavigationItem link="/">Checkout</NavigationItem>
+    <NavigationItem link="/burger-builder" active>Burger Builder</NavigationItem>
+    <NavigationItem link="/checkout">Checkout</NavigationItem>
   </ul>
 );
 
